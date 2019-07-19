64e627de47729eead078c6c0e7afff2f0cf3ede6
Lupe <lupe@winnipeg.internal.amnh.org>
2019-07-19 17:17:00 -0400

    High resolution

--- LoopSnapshots.py
+++ LoopSnapshots.py
@@ -1,30 +1,38 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
 import pynbody 
 import numpy as np
 import pandas as pd
-import matplotlib.pylab as plt
-
-s = pynbody.load("cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096.5.std")
-s.physical_units()
-
+import readcol
+Path = "/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/"
+files = readcol.readcol("/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/files.list")
+files = files[:,0]
+print(files)
 # FUNCTION TO FIND A BLACK HOLE
 def findBH(s):  
     BHfilter = pynbody.filt.LowPass('tform',0.0)
     BH = s.stars[BHfilter]
     return BH
-BH = findBH(s)
-
-pynbody.analysis.angmom.faceon(s)
-
+#BH = findBH(s) until I define s
 def getz(s):
     return s.properties['z']
 
 def gettime(s):
-    return pynbody.analysis.cosmoogy.age(s)
+    return pynbody.analysis.cosmology.age(s)
+pos=[]
+vel=[]
+f = open("LoopSnapshots.dat","w+")
 # Loading Snapshots
-for i in s:
-   
+for i in files:
+    print(i)
+    s  = pynbody.load(Path + i)
+    s.physical_units()
+    BH = findBH(s)
+    pynbody.analysis.angmom.faceon(s)
+    print(BH)
     BHposition = BH['pos'] # POSITION OF THE BLACK HOLE
-    print(BHposition)
+    #print(BHposition)
     BHx = BHposition[:,0]
     print(BHx)
     BHy = BHposition[:,1]
@@ -35,47 +43,82 @@ for i in s:
     print("Magnitude position of the Black Hole: ",posi_magnitude)
     print("Position units: ",s['pos'].units)
 
-# VELOCITY OF THE STARS IN THE GALAXY
-# How many stars?
-number_stars = s.stars[0:]
-total_stars = len(number_stars)
+    # VELOCITY OF THE STARS IN THE GALAXY
+    # How many stars?
+    number_stars = s.stars[0:]
+    total_stars = len(number_stars)
+    print("Total number of stars: ", total_stars)
 # Velocity of the galaxy
-galaxy = s['vel']
-mass = s['mass']
-Galaxy_vx = galaxy[:,0]
-Galaxy_vy = galaxy[:,1]
-Galaxy_vz = galaxy[:,2]
+    galaxy = s['vel']
+    mass = s['mass']
+    Galaxy_vx = galaxy[:,0]
+    Galaxy_vy = galaxy[:,1]
+    Galaxy_vz = galaxy[:,2]
 # Average galaxy velocity
-Av_Vx = np.sum(Galaxy_vx*mass)/(np.sum(mass))
-Av_Vy = np.sum(Galaxy_vy*mass)/(np.sum(mass))
-Av_Vz = np.sum(Galaxy_vz*mass)/(np.sum(mass))
+    Av_Vx = np.sum(Galaxy_vx*mass)/(np.sum(mass))
+    Av_Vy = np.sum(Galaxy_vy*mass)/(np.sum(mass))
+    Av_Vz = np.sum(Galaxy_vz*mass)/(np.sum(mass))
 # Find the velocity of the star 
-vel_1 = Av_Vx - s.stars['vel'][:,0]
-vel_2 = Av_Vy - s.stars['vel'][:,1]
-vel_3 = Av_Vz - s.stars['vel'][:,2]
-print(" Velocity units: ",s['vel'].units)
-magnitude = np.sqrt((vel_1)**2 + (vel_2)**2 + (vel_3)**2)
-for i in s:
+    vel_1 = Av_Vx - s.stars['vel'][:,0]
+    vel_2 = Av_Vy - s.stars['vel'][:,1]
+    vel_3 = Av_Vz - s.stars['vel'][:,2]
+    print(" Velocity units: ",s['vel'].units)
+    magnitude = np.sqrt((vel_1)**2 + (vel_2)**2 + (vel_3)**2)
     Av_vel_stars_in_galaxy =  magnitude.sum()/total_stars
     print("Velocity of stars in the galaxy: ",Av_vel_stars_in_galaxy)
-
 # Find the number of stars around the black hole
-stars = s.stars[0:]
-radius = 0.5
-Sphere = stars[pynbody.filt.Sphere(radius, cen = (0.86460325,-0.36786913,0.1918984))]
-#print(Sphere)
-stars_BH = len(Sphere)
-print("Stars around the Black Hole at 0.5 kpc: ",stars_BH)
-starsv = Sphere['vel']
-
-# VELOCITY OF THE STARS AROUND THE BLACK HOLE
-for i in s:
+    stars = s.stars[0:]
+    radius = 0.5
+    Sphere = stars[pynbody.filt.Sphere(radius, cen = (np.array([BHx[0],BHy[0],BHz[0]])))]
+    #print(Sphere)
+    stars_BH = len(Sphere)
+    print("Stars around the Black Hole at 0.5 kpc: ",stars_BH)
+    starsv = Sphere['vel']
+    # VELOCITY OF THE STARS AROUND THE BLACK HOLE
     vel_stars1 = Av_Vx - starsv[:,0]
     vel_stars2 = Av_Vy - starsv[:,1]
     vel_stars3 = Av_Vz - starsv[:,2]
     magnitude = np.sqrt((vel_1)**2 + (vel_2)**2 + (vel_3)**2)
     Av_vel_around_BH =  magnitude.sum()/total_stars
     print("Velocity of the stars around the Black Hole  ",Av_vel_around_BH)
+    vel.append(Av_vel_around_BH)
+    pos.append(posi_magnitude)
+    data = [s,posi_magnitude, Av_vel_around_BH] 
+        
+    data= str(data)
+    data= data[1:-1]
+    f.write(data+'\n')
+    
+    print(data)
 
+f.close()
 
+    
+numbers = np.array([1,2,10,20])*0.194
 
+# Plot Velocity
+plt.axes()
+plt.title("Velocity vs Time")
+plt.plot(numbers, vel)
+plt.tick_params(axis ="x", labelcolor="r")
+plt.xlabel("Time(Gyr)")
+plt.ylabel("Velocity(km/s)")
+plt.legend()
+plt.show()
+# Plot Position
+plt.title("Position vs Time")
+plt.plot(numbers, pos)
+plt.tick_params(axis = "x", labelcolor="b")
+plt.xlabel("Time(Gyr)")
+plt.ylabel("Position(kpc)")
+plt.legend()
+plt.show()
+'''
+    pynbody.analysis.angmom.faceon(s)
+    # create an image using  the default bands (i, v, u)
+    pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo11Faceon.png')
+
+    # create an image using  the default bands (i, v, u)
+    pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo11Edgeon.png')
+    plt.show()
+'''
